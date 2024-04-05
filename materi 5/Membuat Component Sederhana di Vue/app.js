09f271fbfc40945b1375bbf1a727484d1ecf2bf0
belajar-vue-2 <aryaadi229@gmail.com>
2024-04-05 19:59:31 +0700

    Membuat Component Lebih Kompleks

--- materi 5/Membuat Component Sederhana di Vue/app.js
+++ materi 5/Membuat Component Sederhana di Vue/app.js
@@ -15,11 +15,49 @@ Vue.component('price', {
             type: Number
         },
         precision: {
-            type: String,
+            type: Number,
             default: 2
         }
     },
     template: '<span> {{this.prefix + Number.parseFloat(this.value).toFixed(this.precision)}} </span>'
+})
+
+Vue.component('product-list', {
+    props: ['products', 'maximum'],
+    methods: {
+        before: function (el) {
+            el.className = 'd-none';
+        },
+        enter: function (el) {
+            let delay = el.dataset.index * 100;
+            setTimeout(() => {
+                el.className = 'row d-flex align-items-center justify-content-center animate__animated animate__slideInRight';
+            }, delay);
+        },
+        leave: function (el) {
+            let delay = el.dataset.index * 100;
+            setTimeout(() => {
+                el.className = 'row d-flex align-items-center justify-content-center animate__animated animate__slideOutRight';
+            }, delay);
+        },
+    },
+    template: `<transition-group name="fade" tag="div" @beforeEnter="before" @enter="enter" @leave="leave">
+    <div v-for="(items, index) in products" :data-index="index" :key="items.id"
+        v-if="items.price < Number(maximum)" class="row d-none align-items-center justify-content-center">
+        <div class="col-1 d-flex justify-content-end">
+            <button class="btn rounded-1 btn-primary" v-on:click="addItem(items)">+</button>
+        </div>
+        <div class="col-5 d-flex justify-content-center">
+            <img :src="items.image" alt="items.name" class="w-75">
+        </div>
+        <div class="col-6 d-flex align-items-start justify-content-center flex-column">
+            <h2>{{items.name}}</h2>
+            <p>{{items.description}}</p>
+            <!-- <h3>{{items.price | currencyFormat}}</h3> -->
+            <price :value="Number(items.price)" :prefix="'Rp'" :precision="Number(2)"></price>
+        </div>
+    </div>
+</transition-group>`
 })
 
 var app = new Vue({
@@ -79,21 +117,6 @@ var app = new Vue({
                     qty: 1
                 });
             }
-        },
-        before: function (el) {
-            el.className = 'd-none';
-        },
-        enter: function (el) {
-            let delay = el.dataset.index * 100;
-            setTimeout(() => {
-                el.className = 'row d-flex align-items-center justify-content-center animate__animated animate__slideInRight';
-            }, delay);
-        },
-        leave: function (el) {
-            let delay = el.dataset.index * 100;
-            setTimeout(() => {
-                el.className = 'row d-flex align-items-center justify-content-center animate__animated animate__slideOutRight';
-            }, delay);
         },
         del: function (index) {
             if (this.cart[index].qty > 1) {
