dedee8a7b733171b05d475fb9d004f6092042b7e
belajar-vue-2 <aryaadi229@gmail.com>
2024-04-22 16:57:01 +0700

    vue-cli

---
+++ Materi 6/vue-v2/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="container mt-5">
+      <h2>IDShop</h2>
+      <product-list :products="products" :maximum="maximum"></product-list>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Price from './components/Price.vue';
+import ProductList from './components/ProductList.vue';
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      maximum : 50,
+      products : []
+    }
+  },
+  mounted: function () {
+    fetch('https://hplussport.com/api/products/order/price')
+      .then(res => res.json())
+      .then(res => {
+        this.products = res;
+      })
+  },
+  components: {
+    Price,
+    ProductList
+  }
+}
+</script>
---
+++ Materi 6/vue-v2/src/components/ProductList.vue
@@ -0,0 +1,54 @@
+<template>
+    <transition-group name="fade" tag="div" @beforeEnter="before" @enter="enter" @leave="leave">
+        <div v-for="(items, index) in showItem" :data-index="index" :key="items.id"
+            class="row d-none align-items-center justify-content-center">
+            <div class="col-1 d-flex justify-content-end">
+                <button class="btn rounded-1 btn-primary" @click="$emit('add', items)">+</button>
+            </div>
+            <div class="col-5 d-flex justify-content-center">
+                <img :src="items.image" alt="items.name" class="w-75">
+            </div>
+            <div class="col-6 d-flex align-items-start justify-content-center flex-column">
+                <h2>{{ items.name }}</h2>
+                <p>{{ items.description }}</p>
+                <price :value="Number(items.price)"></price>
+            </div>
+        </div>
+    </transition-group>
+</template>
+
+<script>
+
+import Price from './Price.vue';
+
+export default {
+    name: 'product-list',
+    components: {
+        Price
+    },
+    props: ['products', 'maximum'],
+    computed: {
+        showItem: function () {
+            let max = this.maximum;
+            return this.products.filter(items => items.price <= max);
+        }
+    },
+    methods: {
+        before: function (el) {
+            el.className = 'd-none';
+        },
+        enter: function (el) {
+            let delay = el.dataset.index * 100;
+            setTimeout(() => {
+                el.className = 'row d-flex align-items-center justify-content-center animate__animated animate__slideInRight';
+            }, delay);
+        },
+        leave: function (el) {
+            let delay = el.dataset.index * 100;
+            setTimeout(() => {
+                el.className = 'row d-flex align-items-center justify-content-center animate__animated animate__slideOutRight';
+            }, delay);
+        },
+    }
+}
+</script>
